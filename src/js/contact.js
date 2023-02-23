ef5998c28f23b2439257caca1c387968d5b02438
Bjerkeset <112175751+Bjerkeset@users.noreply.github.com>
2023-02-23 12:59:43 +0100

    fetched data succesfully in index.html
    added form to contact.html.
    
    contact.js got elements DOM.
    
    working on imported SDK to app.js.

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,11 +1,46 @@
+// NOTE: Get DOM elements from the DOM
+const formElement = document.querySelector('#form__js');
+const emailElement = document.querySelector('#email__js');
+const passwordElement = document.querySelector('#password__js');
+const nameElement = document.querySelector('#name__js');
+
+
+// NOTE: Create event listeners for the form
+formElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = emailElement.value;
+    const password = passwordElement.value;
+    const name = nameElement.value;
+
+    
+})
+
+// NOTE: Create a function to validate an input field
+// NOTE: Create a function to create a DOM element
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 ============================================
 Constants
 @example: https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/advanced-form.html#L50
 ============================================
 */
-
 // TODO: Get DOM elements from the DOM
+
+
 
 // TODO: Create event listeners for the form
 
